81b89da1e65bbfd4a0eb12b55b91d6892ee39975
m-ghozi <ghozi286@gmail.com>
2026-04-14 14:52:53 +0700

    ✨ feat: implement security enhancements with helmet, rate limiting, and API key authentication

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const presensiRoutes = require('./routes/presensiRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const API_KEY = process.env.X_API_KEY;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(helmet())
 app.use(express.json());
 
+const apiKeyMiddleware = (req, res, next) => {
+    const userApiKey = req.header('X-API-KEY');
+    if (userApiKey && userApiKey === API_KEY) {
+        next();
+    } else {
+        res.status(403).json({ success: false, message: 'Unauthorized access' });
+    }
+};
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
 // Routes
-app.use('/api', presensiRoutes);
+app.use('/api', limiter, apiKeyMiddleware, presensiRoutes);
 
 // Cek koneksi server
 app.listen(PORT, () => {
